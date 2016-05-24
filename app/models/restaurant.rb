343f3539deeb36789c2ec3cefb9795a96c833241
Tess Ryan <tesstryan@gmail.com>
2016-05-24 12:23:44 -0500

    Allows blank for rating

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,7 @@
 class Restaurant < ActiveRecord::Base
 
     validates :name, presence: true
-    validates :rating, numericality: true, :inclusion => 0..5
+    validates :rating, numericality: true, :inclusion => 0..5, allow_blank: true
 
     has_many :menu_items
 
